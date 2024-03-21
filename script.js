4c815e66808056a960a99249d2ee7240079302b3
mariamnebb <153996932+mariamnebb@users.noreply.github.com>
2024-03-21 22:11:40 +0400

    add second html

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-"use strict"
+"use strict";
 
 
 let btnElement = document.getElementById('clicbtn');
 btnElement.addEventListener('click', function(){
     alert('მადლობა, თქვენი განაცხადი მიღებულია.')
 })
+
+// burgerbar
+
+// let burger = document.getElementById('burger-bar');
+// burger.addEventListener('click',function(){
+//     burger-bar.classlist.add('sign-activen')
+
+// })
