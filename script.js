492b6d44f884e0584be25e8f628cab4a0f7127e8
mariamnebb <153996932+mariamnebb@users.noreply.github.com>
2024-03-21 18:06:07 +0400

    add signin

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-'use strict'
+"use strict"
+
+
+let btnElement = document.getElementById('clicbtn');
+btnElement.addEventListener('click', function(){
+    alert('მადლობა, თქვენი განაცხადი მიღებულია.')
+})
