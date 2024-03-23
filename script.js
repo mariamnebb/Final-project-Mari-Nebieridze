a33c5d2a34e40736a0ae3d9fad22963a996a29cb
mariamnebb <153996932+mariamnebb@users.noreply.github.com>
2024-03-23 20:37:46 +0400

    add js validation

--- script.js
+++ script.js
@@ -152,4 +152,13 @@ function handleClick(e) {
   moveTo.move(scrollElement);
 }
   
-  
+//   validation
+const email = document.getElementById("mail");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I am expecting an email address!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
