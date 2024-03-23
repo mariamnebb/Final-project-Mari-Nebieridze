7d7735649fd97e6bd91ddd97871fbee78d9daa78
mariamnebb <153996932+mariamnebb@users.noreply.github.com>
2024-03-23 19:52:57 +0400

    add a move to library  to npm

--- script.js
+++ script.js
@@ -132,3 +132,24 @@ window.onclick = function(event) {
 //     createPost(newObj);
 //   });
 // });
+//     
+
+//move to (npM)
+
+  const moveTo = new MoveTo();
+const linkArray = document.querySelectorAll(".atr");
+
+
+linkArray.forEach((item) => {
+  item.addEventListener("click", handleClick);
+});
+
+function handleClick(e) {
+  e.preventDefault();
+
+  console.log(this); 
+  const scrollElement = document.querySelector(this.getAttribute("href"));
+  moveTo.move(scrollElement);
+}
+  
+  
