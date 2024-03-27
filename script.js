6f40e40d3d1ae69dffc86788f76251df4904551d
mariamnebb <153996932+mariamnebb@users.noreply.github.com>
2024-03-27 22:28:11 +0400

    final.version

--- script.js
+++ script.js
@@ -19,125 +19,37 @@ btnElement.addEventListener('click', function(){
 
 
 
+// 1. xml http requist
+function ajax() {
+  let requist = new XMLHttpRequest();
+  requist.open("GET", "https://reqres.in/api/users?page=2");
 
+  // წარმატება
+  requist.addEventListener("load", function () {
+    let response = this.responseText; 
+    let responseJs = JSON.parse(response); 
+    console.log(responseJs);
 
+    let ul = document.createElement("ul");
+    let li = document.createElement("li");
+    li.textContent = responseJs.data[1].email;
+    ul.appendChild(li);
 
-// Get the modal
-// let modal = document.getElementById('id01');
-
-
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
-
-// variables ajax
-let mainDiv = document.getElementById("postMainBlock");
-let postOverLay = document.getElementById("overlay");
-let contentDiv = document.getElementById("content");
-let closeIcon = document.getElementById("close");
-let add = document.getElementById("add");
-let overlayAdd = document.getElementById("overlay-add");
-let form = document.getElementById("formElement");
-
-function ajax(url, callback) {
-  fetch('https://jsonplaceholder.typicode.com/', {
-    method: "GET",
-  })
-    .then(function (response) {
-      if (!response.ok) {
-        throw new Error();
-      }
-      return response.json();
-    })
-    .then(function (responseData) {
-      callback(responseData);
-    })
-    .catch(function (error) {
-      alert(error);
-    });
-}
-ajax("https://jsonplaceholder.typicode.com/posts", function (data) {
-  data.forEach((element) => {
-    createPost(element);
-  });
-});
-
-function createPost(item) {
-  let divWraper = document.createElement("div");
-  divWraper.classList.add("post-wraper");
-  divWraper.setAttribute("data-id", item.id);
-
-  let h3Title = document.createElement("h3");
-  h3Title.innerText = item.id;
-
-  let h2Title = document.createElement("h2");
-  h2Title.innerText = item.title;
-
-  let deleteBtn = document.createElement("button");
-  deleteBtn.innerText = "delete this post";
-  deleteBtn.setAttribute("data-delete-id", item.id);
-
-  divWraper.appendChild(h3Title);
-  divWraper.appendChild(h2Title);
-  divWraper.appendChild(deleteBtn);
-
-  deleteBtn.addEventListener("click", function (e) {
-    e.stopPropagation();
-    let deleteId = e.target.getAttribute("data-delete-id");
-    let deleteUrl = `https://jsonplaceholder.typicode.com/posts/${deleteId}`;
-
-    fetch(deleteUrl, {
-      method: "DELETE",
-    }).then(() => divWraper.remove());
+    document.getElementById("api").appendChild(ul);
   });
 
-  divWraper.addEventListener("click", function () {
-    contentDiv.innerHTML = " ";
-    postOverLay.classList.add("active");
-    let id = this.getAttribute("data-id");
-    let newUrl = `https://jsonplaceholder.typicode.com/posts/${id}`;
-    ajax(newUrl, function (newData) {
-      let p = document.createElement("p");
-      p.innerText = newData.body;
-      contentDiv.appendChild(p);
-    });
-  });
+  // წარუმატებელი პასუხი
+  requist.addEventListener("error", function () {
+    let p = document.createElement("p");
+    p.textContent = "Server Error";
 
-  mainDiv.appendChild(divWraper);
+    document.getElementById("api").appendChild(p);
+  });
+  requist.send();
 }
 
-closeIcon.addEventListener("click", function () {
-  postOverLay.classList.remove("active");
-});
-
-add.addEventListener("click", function () {
-  overlayAdd.classList.add("overlayActive");
-});
-
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  let formData = {
-    title: event.target[0].value,
-  };
-
-  fetch("https://jsonplaceholder.typicode.com/posts", {
-    method: "POST",
-    body: JSON.stringify(formData),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((response) => response.json())
-    .then((newObj) => {
-      console.log(newObj);
-      overlayAdd.classList.remove("overlayActive");
-      event.target[0].value = " ";
-      createPost(newObj);
-    });
-});
+ajax();
+// ვალიდაცია
 
 function validateForm() {
     
@@ -168,8 +80,8 @@ function validateEmail(email) {
   return re.test(email);
 }
 // mOVE TO NPm
-const moveTo = new MoveTo();
-const linkArray = document.querySelectorAll(".atr");
+let moveTo = new MoveTo();
+let linkArray = document.querySelectorAll(".atr");
 
 
 linkArray.forEach((item) => {
